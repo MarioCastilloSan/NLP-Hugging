9931515cc3c0f06ec2d8d4e8d06c7f71c0363c2e
Mario <mariocastillosan@gmail.com>
2022-09-26 20:00:34 -0300

    clean parser

--- Transformers/main.py
+++ Transformers/main.py
@@ -1,10 +1,9 @@
 import argparse
 #MAIN ARGUMENTS
 parser= argparse.ArgumentParser()
-parser.add_argument('-en', dest='evalue',action='store_true')
-parser.add_argument('--en', dest='evalue', action='store_false')
-parser.add_argument('-sp', dest='spvalue',action='store_true')
-parser.add_argument('--sp', dest='spvalue', action='store_false')
+parser.add_argument('-en', dest='evalue',action='store_true',required=False)
+parser.add_argument('-sp', dest='spvalue',action='store_true',required=False)
+
 
 #ENGLISH TRANSFORMER ARGUMENTS
 parser.add_argument('-q',dest='qvalue', help="Question", type=str)
@@ -18,12 +17,10 @@ args=parser.parse_args()
         Brief documentation about the arguments parser
          Arguments : 
          -en select the English Transformer for use it.
-         --en indicates the you dont use the English Transformer.
          In case you use it it requiers the following values:
          -q is the question for the model
          -c is the context for the model
          -sp select the Spanish Transformer for use it.
-         --sp indicates the you dont use the Spanish Transformer.
             In case you use it it requiers the following values:
          -qsp is the question for the model
          -csp is the context for the model
